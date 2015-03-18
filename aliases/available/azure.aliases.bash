996e03959c09018504f04291ec7c0380484d462e
Sasha Vinčić <sasha@vincic.org>
2015-03-18 15:48:02 +0100

    azure aliases

---
+++ aliases/available/azure.aliases.bash
@@ -0,0 +1,19 @@
+cite 'about-alias'
+about-alias 'azure cli abbreviations'
+
+# azure account
+alias az='azure'
+alias azal='azure account list'
+alias azas='azure account set'
+
+# vm
+alias azv='azure vm'
+alias azvc='azure vm create'
+alias azvdc='azure vm docker create'
+alias azvl='azure vm list'
+alias azvstart='azure vm start'
+alias azvstop='azure vm stop'
+
+# vm endpoint
+alias azvel='azure vm endpoint list'
+alias azvec='azure vm endpoint create'
